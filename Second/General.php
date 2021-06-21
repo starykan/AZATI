8b10314afa487823fef5d8f1ab9e0f8c7b9658c9
staryk <staryk180289@gmail.com>
2021-06-21 21:32:08 +0300

    It's working!

--- Second/General.php
+++ Second/General.php
@@ -7,45 +7,36 @@ $query = $conn->prepare('SELECT id, speciality FROM specialty');
 $query->execute();
 $specialityList = $query->fetchAll(\PDO::FETCH_ASSOC);
 
-
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
-        // TODO: validate fields
-        // TODO: handle error for file upload
-        // TODO: trim skill list
-        // TODO: make nice structure
+        // TODO: validate fields length
+        // TODO: rename tables and columns
 
-        //====================================== BEGIN VALIDATION
         $errorMessage = null;
-        if () {
-
-        }
-        if () {
-
+        if (empty($_POST['Name']) || empty($_POST['Family']) || empty($_POST['Patronymic']) || empty($_POST['Specialty']) || empty($_POST['Skills'])) {
+            $errorMessage = "Не все данные введены.";
         }
         if ($_FILES['inputfile']['error'] != UPLOAD_ERR_OK || $_FILES['inputfile']['type'] != 'image/jpeg') {
-            //            switch ($_FILES['inputfile']['error']) {
-            //                case UPLOAD_ERR_FORM_SIZE:
-            //                case UPLOAD_ERR_INI_SIZE:
-            //                    echo 'File Size exceed';
-            //                    brake;
-            //                case UPLOAD_ERR_NO_FILE:
-            //                    echo 'FIle Not selected';
-            //                    break;
-            //                default:
-            //                    echo 'Something is wrong';
-            //            }
+            $errorMessage = "Загрузите файл .jpeg.";
         }
-        //====================================== END VALIDATION
-
+        $rawSkillList = $_POST['Skills'];
+        $skillListRaw = explode(",", $rawSkillList);
+        $skillList = [];
+        foreach ($skillListRaw as $skill) {
+            $skill = trim($skill);
+            if (!empty($skill)) {
+                $skillList[] = $skill;
+            }
+        }
+        if (empty($skillList)) {
+            $errorMessage = 'введите навыки через запятую';
+        }
         if (null === $errorMessage) {
             $filename = uniqid() . '.jpg';
             $targetPath = dirname(__FILE__) . '/uploads/' . $filename;
             if (!move_uploaded_file($_FILES['inputfile']['tmp_name'], $targetPath)) {
                 throw new \Exception('file not uploaded');
             }
-
 
             $stmt = $conn->prepare("INSERT INTO people (name, family, patronymic, id_specialty, path) VALUE (:name, :family, :patronymic, :id_specialty, :path)");
 
@@ -63,9 +54,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             ]);
             $peopleId = $conn->lastInsertId();
 
-            $rawSkillList = $_POST['Skills'];
-            $skillList = explode(",", $rawSkillList);
-            var_dump($skillList);
             $skillIds = [];
             $wordInsertStmt = $conn->prepare('INSERT INTO skill (skill) VALUE (:skill)');
             $selectQuery = $conn->prepare('SELECT id FROM skill WHERE skill = :skill');
@@ -95,14 +83,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>HTML-форма изменения новых записей</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>table1</title>
 </head>
 <body>
 <form action = "General.php" method="post" enctype="multipart/form-data">
+    <?php
+    if ($errorMessage !== null){
+        echo $errorMessage;
+    }
+    ?>
     <table>
-        <tr><td><label for="Name">Имя</label></td>
+        <tr>
+            <td><label for="Name">Имя</label></td>
             <td>
                 <input id="Name" name="Name" placeholder="Имя">
             </td>
@@ -133,8 +126,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <tr><td colspan=2><input type="submit" value="Ввод"></td></tr>
     </table>
 </form>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
---
+++ Second/Output.php
@@ -0,0 +1,47 @@
+<?php
+
+$conn = new PDO('mysql:host=localhost;dbname=Azati', 'root', '123');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+if (empty($_GET['id'])) {
+    exit('no id');
+}
+$id = $_GET['id'];
+
+$query = $conn->prepare('SELECT people.*, specialty.speciality, skill.skill
+FROM people
+	JOIN specialty ON specialty.id = people.id_specialty
+    join people_skill on people_skill.id_people = people.id
+    join skill on skill.id = people_skill.id_skill
+
+WHERE
+    people.id = :id');
+$query->execute(['id' => $id]);
+$personData = $query->fetchAll(\PDO::FETCH_ASSOC);
+if (empty($personData)) {
+    exit('no person');
+}
+?>
+<html>
+<head>
+    <title>HTML-форма изменения новых записей</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>table1</title>
+</head>
+<body>
+<img src="/uploads/<?php echo $personData[0]['path']; ?>"><br>
+<b>Имя: </b><?php echo $personData[0]['name']; ?><br>
+<b>Фамилия: </b><?php echo $personData[0]['family']; ?><br>
+<b>Отчество: </b><?php echo $personData[0]['patronymic']; ?><br>
+<b>Специальность: </b><?php echo $personData[0]['speciality']; ?><br>
+<b>Навыки: </b>
+<ul>
+<?php foreach ($personData as $skill) {?>
+    <li><?php echo $skill['skill'] ?></li>
+<?php }; ?>
+</ul>
+</body>
+</html>
