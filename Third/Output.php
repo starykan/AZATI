f9d80e1d22c17e1142b268a93e4aba7d53fef9a3
staryk <staryk180289@gmail.com>
2021-06-27 20:44:35 +0300

    простыня

--- Third/Output.php
+++ Third/Output.php
@@ -1,5 +1,5 @@
 <?php
-
+//TODO: CSS
 $conn = new PDO('mysql:host=localhost;dbname=Azati', 'root', '123');
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -29,7 +29,6 @@ if (empty($personData)) {
 ?>
 <html>
 <head>
-    <script defer src="/js/jquery-3.6.0.js"></script>
     <title>HTML-форма изменения новых записей</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -37,28 +36,10 @@ if (empty($personData)) {
     <title>table1</title>
 </head>
 <body>
-<img src="/uploads/<?php echo $personData[0]['path']; ?>"><br>
-<b>Имя: </b><?php echo $personData[0]['name']; ?><br>
-<b>Фамилия: </b><?php echo $personData[0]['family']; ?><br>
-<b>Отчество: </b><?php echo $personData[0]['patronymic']; ?><br>
-<b>Специальность: </b><?php echo $personData[0]['speciality']; ?><br>
-<b>Навыки: </b>
-<ul>
-<?php foreach ($personData as $skill) {?>
-    <li><?php echo $skill['skill'] ?></li>
-<?php }; ?>
-    <?php var_dump($personData);?>
-</ul>
-</body>
-</html>
-
-
-<form action = "General.php" method="post" enctype="multipart/form-data">
-    <?php
-//    if ($errorMessage !== null){
-//        echo $errorMessage;
-//    }
-    ?>
+<img id="Picture" src="/uploads/<?php echo $personData[0]['path']; ?>"><br>
+<form action = "/api/PutPerson.php" method="post" enctype="multipart/form-data" id="person_form">
+    <div id="messages" hidden></div>
+    <input type="hidden" id="Id" name="Id" placeholder="Id" value="<?php echo htmlentities($personData[0]['id']); ?>">
     <table>
         <tr>
             <td><label for="Name">Имя</label></td>
@@ -78,9 +59,9 @@ if (empty($personData)) {
                 <select id="Speciality" name="Speciality">
                     <?php foreach ($specialityList as $speciality) { ?>
                         <option
-                                <?php if ($personData[0]['id_speciality']==$speciality['id']) { ?>
-                                    selected="selected"
-                                <?php } ?>
+                            <?php if ($personData[0]['id_speciality']==$speciality['id']) { ?>
+                                selected="selected"
+                            <?php } ?>
                                 value="<?php echo $speciality['id']; ?>"
                         >
                             <?php echo $speciality['speciality']; ?>
@@ -90,7 +71,9 @@ if (empty($personData)) {
             </td></tr>
         <tr><td><label for="Skills">Навыки</label></td>
             <td>
-                <p><textarea id="Skills" rows="10" cols="30" name="Skills"></textarea></p>
+                <p><textarea id="Skills" rows="10" cols="30" name="Skills"><?php
+                        echo implode(', ', array_column($personData, 'skill'));
+                        ?></textarea></p>
             </td></tr>
         <tr><td><label for="inputfile">Загрузка файла</label></td>
             <td>
@@ -99,3 +82,51 @@ if (empty($personData)) {
         <tr><td colspan=2><input type="submit" value="Ввод"></td></tr>
     </table>
 </form>
+<script>
+var form = document.getElementById('person_form');
+form.onsubmit = async (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const url = form.action;
+    const method = form.method;
+    try {
+        const formData = new FormData(form);
+        fetch(url, {
+            method: method,
+            body: formData
+        })
+        .then(response => {
+            console.log(response);
+            if (response.status !== 200) {
+                return response.json().then(data => {
+                    const messages = document.getElementById("messages");
+                    messages.removeAttribute("hidden");
+                    console.log("message: " + data);
+                    messages.textContent = data.message;
+                });
+            }
+            if (response.status === 200) {
+                return response.json().then(data => {
+                    const messages = document.getElementById("messages");
+                    messages.removeAttribute("hidden");
+                    // messages.setAttribute("hidden", "hidden");
+                    messages.textContent = 'Успешно сохранено!';
+                    document.getElementById("Name").value = data.name;
+                    document.getElementById("Family").value = data.family;
+                    document.getElementById("Patronymic").value = data.patronymic;
+                    document.getElementById("Speciality").value = data.speciality.id;
+                    document.getElementById("Skills").value = data.skills;
+                    document.getElementById("Picture").src = data.imageUrl;
+                    document.getElementById("inputfile").value = null;
+                });
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+</script>
+</body>
+</html>
+
+
---
+++ Third/api/PutPerson.php
@@ -0,0 +1,137 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new PDO('mysql:host=localhost;dbname=Azati', 'root', '123');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header("HTTP/1.1 405 Method Not Allowed");
+    echo json_encode(['message' => '405 Method Not Allowed']);
+    exit;
+}
+if (empty($_POST['Id']) || empty($_POST['Name']) || empty($_POST['Family']) || empty($_POST['Patronymic']) || empty($_POST['Speciality']) || empty($_POST['Skills'])) {
+    $errorMessage = "Не все данные введены.";
+}
+$peopleId = $_POST['Id'];
+$name = $_POST['Name'];
+$family = $_POST['Family'];
+$patronymic = $_POST['Patronymic'];
+$idSpeciality = $_POST['Speciality'];
+
+$picUploaded = !empty($_FILES['inputfile']) /* field exists */ && $_FILES['inputfile']['error'] != UPLOAD_ERR_NO_FILE /* file is uploaded */;
+if ($picUploaded) {
+    if ($_FILES['inputfile']['error'] != UPLOAD_ERR_OK || $_FILES['inputfile']['type'] != 'image/jpeg') {
+        $errorMessage = "Загрузите файл .jpeg.";
+    }
+}
+
+$stmt = $conn->prepare('select * from people where id = :id');
+$stmt->execute(['id' => $peopleId]);
+$oldData = $stmt->fetch();
+if (false === $oldData) {
+    $errorMessage = "Неверный ID.";
+}
+
+if (!empty($errorMessage)) {
+    header("HTTP/1.1 400 Bad Request");
+    echo json_encode(['message' => $errorMessage]);
+    exit;
+}
+$conn->beginTransaction();
+
+if ($picUploaded) {
+    $filename = uniqid() . '.jpg';
+    $targetPath = dirname(dirname(__FILE__)) . '/uploads/' . $filename;
+    if (!move_uploaded_file($_FILES['inputfile']['tmp_name'], $targetPath)) {
+        throw new \Exception('file not uploaded');
+    }
+    $stmt = $conn->prepare('UPDATE people SET name = :name, family = :family, patronymic = :patronymic, id_speciality = :id_speciality, path = :path where id = :id');
+    $stmt->execute([
+        'id' => $peopleId,
+        'name' => $name,
+        'family' => $family,
+        'patronymic' => $patronymic,
+        'id_speciality' => $idSpeciality,
+        'path' => $filename,
+    ]);
+}
+else{
+    $stmt = $conn->prepare('UPDATE people SET name = :name, family = :family, patronymic = :patronymic, id_speciality = :id_speciality where id = :id');
+    $stmt->execute([
+        'id' => $peopleId,
+        'name' => $name,
+        'family' => $family,
+        'patronymic' => $patronymic,
+        'id_speciality' => $idSpeciality,
+    ]);
+}
+
+
+$query = $conn->prepare('SELECT skill.id, skill.skill FROM people_skill JOIN skill on skill.id = people_skill.id_skill where id_people = :id');
+$query->execute(['id' => $peopleId]);
+$skillFromDB = $query->fetchAll();
+$existingSkills = array_column($skillFromDB, 'skill', 'id');
+
+$rawSkillList = $_POST['Skills'];
+$skillListRaw = explode(",", $rawSkillList);
+$newSkills = [];
+foreach ($skillListRaw as $skillName) {
+    $skillName = trim($skillName);
+    if (empty($skillName)) {
+        continue;
+    }
+    $stmt = $conn->prepare('SELECT id FROM skill WHERE skill = :skill');
+    $stmt->execute(['skill' => $skillName]);
+    if ($row = $stmt->fetch()) {
+        $newSkills[$row['id']] = $skillName;
+        continue;
+    }
+    $insertStmt = $conn->prepare('INSERT INTO skill (skill) VALUE (:skill)');
+    $insertStmt->execute(['skill' => $skillName]);
+    $newSkills[$conn->lastInsertId()] = $skillName;
+}
+
+$array1 = array_keys($existingSkills);
+$array2 = array_keys($newSkills);
+$deletingSkills = array_diff($array1, $array2);
+$insertingSkills = array_diff($array2, $array1);
+
+foreach ($deletingSkills as $skillId) {
+    $conn
+        ->prepare('DELETE FROM people_skill where id_skill = :skillId and id_people =:peopleId')
+        ->execute(['peopleId' => $peopleId,'skillId' => $skillId]);
+}
+foreach ($insertingSkills as $skillId){
+    $conn
+        ->prepare('INSERT INTO people_skill (id_people,id_skill) value (:peopleId, :skillId)')
+        ->execute(['peopleId' => $peopleId, 'skillId' => $skillId]);
+}
+
+$conn->commit();
+
+unlink(dirname(dirname(__FILE__)) . '/uploads/' . $oldData['path']);
+
+$query = $conn->prepare('SELECT people.*, specialities.speciality, skill.skill
+FROM people
+	JOIN specialities ON specialities.id = people.id_speciality
+    join people_skill on people_skill.id_people = people.id
+    join skill on skill.id = people_skill.id_skill
+
+WHERE
+    people.id = :id');
+$query->execute(['id' => $peopleId]);
+$personData = $query->fetchAll(\PDO::FETCH_ASSOC);
+
+header("HTTP/1.1 200 OK");
+echo json_encode([
+    'id' => $personData[0]['id'],
+    'name' => $personData[0]['name'],
+    'family' => $personData[0]['family'],
+    'patronymic' => $personData[0]['patronymic'],
+    'speciality' => [
+        'id' => $personData[0]['id_speciality'],
+        'name' => $personData[0]['speciality'],
+    ],
+    'skills' => implode(', ', array_column($personData, 'skill')),
+    'imageUrl' => '/uploads/' . $personData[0]['path'],
+]);
